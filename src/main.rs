07bbcb9d767015ce94b02386c421a5f049069b51
George Marcus <39914804+george-marcus@users.noreply.github.com>
2021-11-11 19:32:24 +0200

    added unit tests, refactored minigrep_configuration.rs to contain Config struct with its implementations as part of the module minigrep_configuration.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use std::env;
 use std::process;
-use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    let config = minigrep::create_config(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
